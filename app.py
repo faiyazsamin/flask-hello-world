1f9d4432daf3312199c826548a897b3b0afe1273
Sheikh Faiyaz Ahmed <faiyaz.samin@gmail.com>
2023-04-11 03:58:41 +0600

    Update

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ from bs4 import BeautifulSoup
 def extractor():
     # target url
     all_data = ''
-    url = ["https://mzamin.com/news.php?news=50698","https://mzamin.com/news.php?news=50671","https://www.jagonews24.com/international/news/846639","https://www.somoynews.tv/news/2023-04-11/প্রতিদিন-দুই-পরিপূর্ণ-রোজার-সওয়াব-লাভ-করবেন-যেভাবে","https://www.prothomalo.com/world/india/zwke9i8cc6","https://www.somoynews.tv/news/2023-04-11/ইউরোপকে-অবশ্যই-যুক্তরাষ্ট্র-নির্ভরশীলতা-কমাতে-হবে-ম্যাক্রোঁ","https://www.ajkerpatrika.com/267868/ডলারের-আধিপত্য-ঠেকাতে-বিকল্প-আনছে-ব্রিকস-জোট"]
+#     url = ["https://mzamin.com/news.php?news=50698","https://mzamin.com/news.php?news=50671","https://www.jagonews24.com/international/news/846639","https://www.somoynews.tv/news/2023-04-11/প্রতিদিন-দুই-পরিপূর্ণ-রোজার-সওয়াব-লাভ-করবেন-যেভাবে","https://www.prothomalo.com/world/india/zwke9i8cc6","https://www.somoynews.tv/news/2023-04-11/ইউরোপকে-অবশ্যই-যুক্তরাষ্ট্র-নির্ভরশীলতা-কমাতে-হবে-ম্যাক্রোঁ","https://www.ajkerpatrika.com/267868/ডলারের-আধিপত্য-ঠেকাতে-বিকল্প-আনছে-ব্রিকস-জোট"]
 #     url = request.args.get('data')
+    data_string = str(request.args.get('data'))
+    url = data_string.split(',')
     # making requests instance
     for items in url:
         reqs = requests.get(items)
